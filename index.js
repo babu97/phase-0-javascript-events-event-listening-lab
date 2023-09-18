383bc93be4a814eeba825b98f345ea3205414012
babu97 <benkaimugul@gmail.com>
2023-09-18 07:15:36 +0300

    finished dom events lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+
+
+    const button =document.getElementById('button');
+    button.addEventListener('click', function(){
+        console.log('button  was clicked!');
+    });
+
+
 }
